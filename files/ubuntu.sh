73e53946e27883d786feb88220412fd110af8d6a
lycheng <lycheng997@gmail.com>
2019-01-09 09:51:58 +0800

    use neovim as vim

--- files/ubuntu.sh
+++ files/ubuntu.sh
@@ -1,39 +1,39 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo add-apt-repository ppa:neovim-ppa/stable
 
-sudo apt-get install git
-sudo apt-get install python-dev
-sudo apt-get install python-pip
-sudo apt-get install silversearcher-ag
-sudo apt-get install xclip
+sudo apt update
+sudo apt upgrade
+
+sudo apt install git
+sudo apt install python-dev
+sudo apt install python-pip
+sudo apt install silversearcher-ag
+sudo apt install xclip
 
 # oh my zsh
-sudo apt-get install zsh
+sudo apt install zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # tmux
-sudo apt-get install tmux
+sudo apt install tmux
 
 # vim
-sudo apt-get install vim-nox
+sudo apt install neovim
 git clone git://github.com/lycheng/dot-vimrc.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim .config/nvim
+ln -s ~/.vimrc .config/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # vim requirements
-sudo apt-get install ack-grep ctags
-sudo apt-get install CMake
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt install ack-grep ctags
+sudo apt install CMake
+sudo apt install nodejs
+sudo apt install npm
 sudo npm install -g jsonlint
 sudo npm install -g csslint
 sudo npm install -g jshint
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 cp files/flake8 $HOME/.config/flake8
-
-# golang
-sudo apt-get install golang
-sudo apt-get install golang-go.tools
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
