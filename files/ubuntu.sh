7eb66129cf7d2a0ca6b49a1544a11b1ecb5bbbdf
lycheng <lycheng997@gmail.com>
2019-04-15 23:21:17 +0800

    update ubuntu init

--- files/ubuntu.sh
+++ files/ubuntu.sh
@@ -6,7 +6,6 @@ sudo apt upgrade
 sudo apt install git
 sudo apt install python-dev
 sudo apt install python-pip
-sudo apt install silversearcher-ag
 sudo apt install xclip
 
 # oh my zsh
@@ -18,8 +17,6 @@ sudo apt install tmux
 
 # neovim
 sudo apt install neovim
-sudo apt install python-neovim
-sudo apt install python3-neovim
 git clone git://github.com/lycheng/dot-vimrc.git ~/.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -28,19 +25,8 @@ ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim .config/nvim
 ln -s ~/.vimrc .config/nvim/init.vim
 
-pip install --user --upgrade pynvim
-pip install --user --upgrade neovim
-
 # vim requirements
-sudo apt install ack-grep ctags
-sudo apt install CMake
-sudo apt install nodejs
-sudo apt install npm
-sudo npm install -g jsonlint
-sudo npm install -g csslint
-sudo npm install -g jshint
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-cp files/flake8 $HOME/.config/flake8
+sudo apt install ack-grep ctags CMake
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
