c646dfcbedf4c0e087e22b2481ae0783cf5441e0
lycheng <lycheng997@gmail.com>
2019-11-11 09:39:55 +0800

    feat: use unstable neovim

--- files/ubuntu.sh
+++ files/ubuntu.sh
@@ -1,22 +1,23 @@
-sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/unstable
 
 sudo apt update
 sudo apt upgrade
 
-sudo apt install git
-sudo apt install python-dev
-sudo apt install python-pip
-sudo apt install xclip
+sudo apt install \
+    git \
+    python-dev \ 
+    python-pip \
+    xclip \
+    zsh \
+    tmux \
+    neovim \
+    ctags \
+    CMake
 
 # oh my zsh
-sudo apt install zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-# tmux
-sudo apt install tmux
-
 # neovim
-sudo apt install neovim
 git clone git://github.com/lycheng/dot-vimrc.git ~/.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -26,8 +27,14 @@ ln -s ~/.vim .config/nvim
 ln -s ~/.vimrc .config/nvim/init.vim
 
 # vim requirements
-sudo apt install ack-grep ctags CMake
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
