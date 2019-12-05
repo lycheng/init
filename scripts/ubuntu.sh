2884264067dfbb637103b0e3701f5db2958c923a
lycheng <lycheng997@gmail.com>
2019-12-06 00:21:47 +0800

    feat: remove kubectl and fzf

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -28,8 +28,8 @@ sudo update-alternatives --config editor
 # extra tools
 
 ## fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+## git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+## ~/.fzf/install
 
 ## tldr
 npm install -g tldr
