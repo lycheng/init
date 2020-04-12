1b7034062ce9ed9c57088f42387821051c918ee7
lycheng <lycheng997@gmail.com>
2020-04-12 13:59:46 +0800

    fix: vim config error

--- vim.sh
+++ vim.sh
@@ -1,17 +1,16 @@
 VERSION=v0.4.3
 BIN_PATH=/usr/bin/nvim
 
-sudo rm ${BIN_PATH}
 sudo curl -fLo ${BIN_PATH} https://github.com/neovim/neovim/releases/download/${VERSION}/nvim.appimage
 sudo chmod a+x ${BIN_PATH}
 
-git clone git://github.com/lycheng/dot-vimrc.git ~/.vim
+git clone git@github.com:lycheng/dot-vimrc.git ~/.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim .config/nvim
-ln -s ~/.vimrc .config/nvim/init.vim
+ 
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim $HOME/.config/nvim
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
 
 sudo update-alternatives --install /usr/bin/vi vi ${BIN_PATH} 60
 sudo update-alternatives --config vi
